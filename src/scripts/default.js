a40ce138916b194066afb553597f38fcbee4115d
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2021-12-20 15:21:54 +0100

    Update code

--- src/scripts/default.js
+++ src/scripts/default.js
@@ -1,23 +1,26 @@
-setTimeout(function start (){
+let trigger_anime_chart = true;
+function animeChart() {
+  if (trigger_anime_chart)
+  {
+    $('.bar').each(function(i){
+      var $bar = $(this);
+      $(this).append('<span class="count"></span>')
+      setTimeout(function(){
+        $bar.css('width', $bar.attr('data-percent'));
+      }, i*100);
+    });
 
-  $('.bar').each(function(i){
-    var $bar = $(this);
-    $(this).append('<span class="count"></span>')
-    setTimeout(function(){
-      $bar.css('width', $bar.attr('data-percent'));
-    }, i*100);
-  });
-
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
+    $('.count').each(function () {
+      $(this).prop('Counter',0).animate({
         Counter: $(this).parent('.bar').attr('data-percent')
-    }, {
+      }, {
         duration: 2000,
         easing: 'swing',
         step: function (now) {
-            $(this).text(Math.ceil(now) +'%');
+          $(this).text(Math.ceil(now) +'%');
         }
+      });
     });
-});
-
-}, 500)
+    trigger_anime_chart = false;
+  }
+}
